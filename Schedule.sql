0ef52600a8fd952198114ce18d436168cdf53d78
PolBoci <polboci0@gmail.com>
2024-10-28 21:23:23 -0400

    Create Schedule.sql

---
+++ Schedule.sql
@@ -0,0 +1,28 @@
+CREATE TABLE tableName (
+    Time	VARCHAR(512),
+    Barber 1	VARCHAR(512),
+    Barber 2	VARCHAR(512),
+    Barber 3	VARCHAR(512),
+    Barber 4	VARCHAR(512)
+);
+
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('9:00', '', '', '', '');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('9:30', 'Booked', 'Booked', 'Booked', 'Booked');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('10:00', 'Booked', 'Booked', 'Booked', 'Booked');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('10:30', 'Booked', 'Booked', 'Booked', 'Booked');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('11:00', '', '', '', 'Booked');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('11:30', '', 'Booked', 'Booked', '');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('12:00', 'Booked', 'Booked', 'Booked', '');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('12:30', 'Booked', 'Booked', 'Booked', '');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('13:00', 'Booked', 'Booked', 'Booked', 'Booked');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('13:30', 'Booked', 'Booked', 'Booked', 'Booked');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('14:00', '', '', 'Booked', 'Booked');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('14:30', '', '', 'Booked', 'Booked');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('15:00', 'Booked', 'Booked', 'Booked', 'Booked');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('15:30', 'Booked', 'Booked', 'Booked', 'Booked');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('16:00', 'Booked', 'Booked', 'Booked', 'Booked');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('16:30', '', '', '', '');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('17:00', '', '', '', '');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('17:30', '', 'Booked', 'Booked', 'Booked');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('18:00', '', 'Booked', 'Booked', 'Booked');
+INSERT INTO tableName (Time, Barber 1, Barber 2, Barber 3, Barber 4) VALUES ('18:30', '', 'Booked', 'Booked', 'Booked');
